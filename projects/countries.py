7f4fee087daea0ab8ce8428aea3d4b52d9a10fc0
afnan <dev.afoyez@gmail.com>
2023-04-27 17:23:48 -0400

    countries.py

--- projects/countries.py
+++ projects/countries.py
@@ -16,9 +16,16 @@ else:
     print("Country found!")
     data = response.json()
     name = data[0]['name']['official']
+    continent = data[0]['continents']
     landlocked = data[0]['landlocked']
     flag = data[0]['flag']
+    flag_des = data[0]['flags']['alt']
     population = data[0]['population']
     formatted_population = '{:.2f}M'.format(population/1000000)
-    print(formatted_population)
+
+    currencies = data[0]['currencies']
+    currency_code = list(currencies.keys())[0] 
+    currency_name = currencies[currency_code]['name']
+    currency_symbol = currencies[currency_code]['symbol']
+    
     
