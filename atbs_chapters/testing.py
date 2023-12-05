347acdf63a0372e5beb15449fad58abb8d39a8f3
afnan <dev.afoyez@gmail.com>
2023-12-05 16:31:08 -0500

    while loop examples

---
+++ atbs_chapters/testing.py
@@ -0,0 +1,23 @@
+'''
+# While Loop Example
+name = ''
+while name != 'your name':
+    print('Please type your name')
+    name = input()
+    if name == 'afnan':
+        print('Hello Afnan!')
+        break
+print('Thank you')
+
+'''
+
+'''
+# While True Loop Example
+while True:
+    print('Please type your name')
+    name = input()
+    if name == 'afnan':
+        print('Hello Afnan!')
+        break
+print('Thank you')
+'''
