fc15f31d8b3962d26bf550bc42ba71ffef0baab7
afnfyz <dev.afoyez@gmail.com>
2023-12-06 10:03:29 -0500

    for loop examples:

--- atbs_chapters/testing.py
+++ atbs_chapters/testing.py
@@ -36,3 +36,33 @@ while True:
     break
 print('Thank you Musa.')
 '''
+'''
+# For Loop Examples
+print ('My name is')
+for i in range(5):
+    print('Jimmy Five Times ' + str(i))
+
+
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
+
+
+print('My name is')
+i = 0
+while i < 5:
+    print('Abdullah Five Times ' + str(i))
+    i = i + 1
+
+for i in range(7,17):
+    print(i)
+
+
+for i in range (1,15,3):
+    print(i)
+
+
+for i in range (15,0,-1):
+    print(i)
+'''
