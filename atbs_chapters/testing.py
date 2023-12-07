597c9ec930baf7585fcdbe843e4dc7ad58a3324c
afnan <dev.afoyez@gmail.com>
2023-12-07 14:32:59 -0500

    Zig Zag Animation

--- atbs_chapters/testing.py
+++ atbs_chapters/testing.py
@@ -185,7 +185,7 @@ print(eggs)
 
 '''
 
-
+'''
 # Error Handling
 
 def bfast(divideby):
@@ -197,3 +197,35 @@ print(bfast(10))
 print(bfast(5))
 print(bfast(0))
 print(bfast(3))
+'''
+
+'''
+# Zig Zag Animation
+
+import sys,time
+
+indent = 0 # How many spaces to indent.
+indentIncreasing = True # Whether the indentation is increasing or not.
+
+try:
+    while True: # The main program loop.
+        print(' ' * indent, end='')
+        print('********')
+        time.sleep(0.08) # Pause for 1/10 of a second.
+
+        if indentIncreasing:
+            # Increase the number of spaces:
+            indent = indent + 5
+            if indent >= 20:
+                # Change direction:
+                indentIncreasing = False
+
+        else:
+            # Decrease the number of spaces:
+            indent = indent - 3
+            if indent <= 0:
+                # Change direction:
+                indentIncreasing = True
+except KeyboardInterrupt:
+    sys.exit()
+'''
