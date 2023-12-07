b621a2b5b10b9e99b823a5c230de726c8871feda
afnan <dev.afoyez@gmail.com>
2023-12-07 15:02:47 -0500

    Collatz Sequence

--- atbs_chapters/testing.py
+++ atbs_chapters/testing.py
@@ -229,3 +229,16 @@ try:
 except KeyboardInterrupt:
     sys.exit()
 '''
+
+number = int(input('Integer: '))
+
+def collatz(number):
+    while number != 1:
+        if number % 2 == 0:
+            number = number // 2
+        else:
+            number = 3 * number + 1
+        print(number)
+
+collatz(number)
+
